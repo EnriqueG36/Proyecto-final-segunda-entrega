ffe84cfeff560b0b8f1558690a6d0f020cf3eaf3
EnriqueG36 <loce0368@gmail.com>
2022-11-19 19:59:38 -0600

    Mongo db funcion de añadir productos a un carrito por su id, funcionando

--- models/daos/carritos/carritos.firebase.dao.js
+++ models/daos/carritos/carritos.firebase.dao.js
@@ -1,12 +1,35 @@
 //Data Access Object para productos, en firebase
 
 const FirebaseContainer = require("../../containers/firebase.container.js");
+const ProductosFirebaseDao = require ('../productos/productos.firebase.dao');       //Importamos el dao de firebase
+const productosFireBaseDao = new ProductosFirebaseDao();
 
 const collection = "carritos";
 class ProductsFirebaseDao extends FirebaseContainer {
   constructor() {
     super(collection);
   }
+
+  async addProductToCart (idcart, idproduct){
+
+    const {id} = idproduct;
+
+    console.log (idcart, id);
+    const buscarProducto = await productosFireBaseDao.getById(id);        //Obtenemos el producto buscando su id mediante un metodo del dao de productos
+    console.log (buscarProducto);
+    
+    const docRef = this.query.doc(idcart);
+    //const document = await docRef.get();
+    //return document.data();
+    //let objetoCualquiera = document.data();
+    //objetoCualquiera.productos = buscarProducto;
+    return await docRef.update({productos: buscarProducto});
+
+    
+
+
+  }
+
 }
 
 module.exports = ProductsFirebaseDao;
--- models/daos/carritos/carritos.mongo.dao.js
+++ models/daos/carritos/carritos.mongo.dao.js
@@ -1,6 +1,6 @@
 // data access objet para la coleccion carritos en la db mongo atlas
 
-const { Schema } = require('mongoose');
+const { Schema, Mongoose } = require('mongoose');
 const MongoContainer = require('../../containers/mongo.container');
 const  ProductosMongoDao  = require('../productos/productos.mongo.dao.js');
 const productosMongoDao = new ProductosMongoDao();
@@ -11,7 +11,18 @@ const collection = "carritos";                            //coleccion carritos
 const carritosSchema = new Schema({                       //squema de mongoose para la coleccion de carritos
   id: { type: Number},
   timeStamp: { type: Date, default: Date.now()},
-  productos: [{}],
+  productos: [{
+    _id: { type: Schema.Types.ObjectId},
+    id: { type: Number },
+    timeStamp: { type: Date, default: Date.now()},
+    nombre: { type: String },
+    descripcion: { type: String},
+    codigo: { type: Number },
+    foto: { type: String },
+    precio: { type: Number },
+    stock: { type: Number }},
+    
+  ]
 });
 
 class CarritosMongoDao extends MongoContainer {           //carritosMongoDao hereda de la clase MongoContainer
@@ -21,21 +32,23 @@ class CarritosMongoDao extends MongoContainer {           //carritosMongoDao her
   
   async addProductToCart(idCarrito, idProducto) {         //Metodo para añadir un producto de la bd a un carrito por su id
 
-    console.log(idCarrito, idProducto);
+    //console.log(idCarrito, idProducto);
 
     const { id } = idProducto;
 
-    console.log(id);                                              //Aquí compruebo que esté recibiendo el id del producto
+    //console.log(id);                                              //Aquí compruebo que esté recibiendo el id del producto
 
     const buscarProducto = await productosMongoDao.getById(id);
 
-    console.log(buscarProducto);                                  //Aquí se comprueba que sí est'a leyendo el producto de la bd por su id
+    //console.log(buscarProducto);                                  //Aquí se comprueba que sí est'a leyendo el producto de la bd por su id
   
-    this.model.updateOne({ id: idCarrito }, {
-      $push: {
-        productos: [buscarProducto]
-      }
-    })
+    //const verElCarrito = await this.getById(idCarrito);
+    //console.log (verElCarrito);
+
+    //this.model.updateOne( { id: idCarrito }, { $set: [ {id: 10}] } )
+    const updatedCarrito = await this.model.updateOne( { id: idCarrito }, { $push: {productos: buscarProducto} } )
+
+    return updatedCarrito;
   } 
 }
 
